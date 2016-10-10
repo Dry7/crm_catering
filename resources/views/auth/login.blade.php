3aa07f44a626e30064be93ed77b2d53c6e6ffdf6
Dry7 <andrey.dry7.shilov@yandex.ru>
2016-10-10 19:37:34 +0300

    Login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,12 +8,12 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="{{ url('/home') }}"><b>Admin</b>LTE</a>
-        </div><!-- /.login-logo -->
+            <img src="/img/logo.png" />
+        </div>
 
     @if (count($errors) > 0)
         <div class="alert alert-danger">
-            <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
+            <strong>Ошибки</strong><br><br>
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
@@ -31,35 +31,28 @@
     @endif
 
     <div class="login-box-body">
-    <p class="login-box-msg"> {{ trans('adminlte_lang::message.siginsession') }} </p>
     <form action="{{ url('/login') }}" method="post">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="{{ trans('adminlte_lang::message.email') }}" name="username"/>
+            <input type="text" class="form-control" placeholder="Логин" name="username"/>
         </div>
         <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="{{ trans('adminlte_lang::message.password') }}" name="password"/>
+            <input type="password" class="form-control" placeholder="Пароль" name="password"/>
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
         <div class="row">
             <div class="col-xs-8">
                 <div class="checkbox icheck">
                     <label>
-                        <input type="checkbox" name="remember"> {{ trans('adminlte_lang::message.remember') }}
+                        <input type="checkbox" name="remember"> Запомнить меня
                     </label>
                 </div>
-            </div><!-- /.col -->
+            </div>
             <div class="col-xs-4">
-                <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.buttonsign') }}</button>
-            </div><!-- /.col -->
+                <button type="submit" class="btn btn-primary btn-block btn-flat">Войти</button>
+            </div>
         </div>
     </form>
-
-    @include('auth.partials.social_login')
-
-    <a href="{{ url('/password/reset') }}">{{ trans('adminlte_lang::message.forgotpassword') }}</a><br>
-    <a href="{{ url('/register') }}" class="text-center">{{ trans('adminlte_lang::message.registermember') }}</a>
-
 </div>
 
 </div>
