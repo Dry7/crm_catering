e24b26ec81d21ad904a19bdf5519f2babdd9faa0
Dry7 <andrey.dry7.shilov@yandex.ru>
2016-11-01 10:13:49 +0300

    Events seed

--- database/seeds/EventSeeder.php
+++ database/seeds/EventSeeder.php
@@ -12,6 +12,6 @@ class EventSeeder extends Seeder
     public function run()
     {
         \DB::table('events')->truncate();
-        factory(\App\Models\Event::class, 1000)->create();
+        factory(\App\Models\Event::class, 30)->create();
     }
 }
