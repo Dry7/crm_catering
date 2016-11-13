9f6626c86528dc07bec12a3bd3d44253a0af8de8
Dry7 <andrey.dry7.shilov@yandex.ru>
2016-11-13 21:31:59 +0300

    Hide calendar

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:Dry7/crm_catering.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, :master
+set :branch, :develop
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/var/www/catering'
