37a7887972040b56647f48f1e3ed46db4cf1d044
Andrey Shilov <andrey.dry7.shilov@yandex.ru>
2020-09-19 18:49:18 +0300

    Downgrade php version, add .env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
     {
         ini_set("pcre.backtrack_limit", "23001337");
         ini_set("pcre.recursion_limit", "23001337");
+        if (env('APP_ENV') === 'production') {
+            URL::forceSchema('https');
+        }
     }
 
     /**
